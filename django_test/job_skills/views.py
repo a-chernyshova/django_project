eda403b8104f6ec362c599758a47231d5c88d6df
nassy <stasya.aska@gmail.com>
2017-01-05 12:26:44 -0500

    connected css, template, rendered index.html

--- django_test/job_skills/views.py
+++ django_test/job_skills/views.py
@@ -1,4 +1,5 @@
 from django.http import HttpResponse
+from django.shortcuts import render
 # Create your views here.
 def index(request):
     return HttpResponse("<h1>Don't worry, learn math part</h1>")
--- django_test/main_app/views.py
+++ django_test/main_app/views.py
@@ -2,4 +2,10 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 def index(request):
-    return HttpResponse("<h1>Wake up, Neo!</h1>")
+    name = "blablabla"
+    num = 1000
+    context = {
+        "name": name,
+        "num": num}
+    #return HttpResponse("<h1>Wake up, Neo!</h1>")
+    return render(request, "index.html", context)
